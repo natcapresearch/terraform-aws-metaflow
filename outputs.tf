3e79d743fa5bceba0737f9a7c91a5de3318f43b5
Simon Hill <simon.hill@natcapresearch.com>
2025-06-24 16:25:20 +0100

    Update output list

--- outputs.tf
+++ outputs.tf
@@ -132,3 +132,7 @@ output "batch_compute_environment_security_group_id" {
 output "datastore_bucket_id" {
   value = module.metaflow-datastore.s3_bucket_id
 }
+
+output "elb_access_logs_bucket_name" {
+  value = aws_s3_bucket.elb_access_logs_bucket[0].bucket
+}
