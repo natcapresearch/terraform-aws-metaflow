0ec5e52931020af4bf8eb82736d12ba47a7af916
Richard Davies <richard.davies@natcapresearch.com>
2025-09-22 12:09:46 +0100

    IN-414: tf fmt and docs

--- modules/metadata-service/versions.tf
+++ modules/metadata-service/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "~> 2.7"
     }
   }
