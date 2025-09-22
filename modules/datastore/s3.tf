e149cd82e86278e2c2cf9a83a761fe488643a8d1
Richard Davies <richard.davies@natcapresearch.com>
2025-09-22 11:44:20 +0100

    IN-414: solve S3 warnings

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -1,16 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket        = local.s3_bucket_name
-  acl           = "private"
   force_destroy = var.force_destroy_s3_bucket
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-      bucket_key_enabled = var.bucket_key_enabled
-    }
-  }
 
   tags = merge(
     var.standard_tags,
@@ -19,6 +9,24 @@ resource "aws_s3_bucket" "this" {
       Metaflow = "true"
     }
   )
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl           = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+
+    bucket_key_enabled = var.bucket_key_enabled
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
