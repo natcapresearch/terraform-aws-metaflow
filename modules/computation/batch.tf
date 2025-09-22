bf513bab649f5e02b9f0b22af89bc0620036eec8
Richard Davies <richard.davies@natcapresearch.com>
2025-09-22 11:44:57 +0100

    IN-414: fix aws provder 6 change

--- modules/computation/batch.tf
+++ modules/computation/batch.tf
@@ -6,7 +6,8 @@ resource "aws_batch_compute_environment" "this" {
      just used compute_environment_name, then there would be a conflict when we went to stand up the new
      compute_environment that had the modifications applied and the process would fail.
   */
-  compute_environment_name_prefix = local.compute_env_prefix_name
+  # AWS provider >= 6.0 changes the name of this parameter to name_prefix
+  name_prefix = local.compute_env_prefix_name
 
   # Give permissions so the batch service can make API calls.
   service_role = aws_iam_role.batch_execution_role.arn
