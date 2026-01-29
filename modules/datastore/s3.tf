ead04e84786fc5f2f4d49f9d3f7fd7eb40ce409e
rdaviesuk <richard.davies@natcapresearch.com>
2026-01-29 14:43:52 +0000

    IN-486: fix S3 ACL for new buckets (#36)

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -13,24 +13,6 @@ resource "aws_s3_bucket" "this" {
   )
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  access_control_policy {
-    grant {
-      grantee {
-        id   = data.aws_canonical_user_id.current.id
-        type = "CanonicalUser"
-      }
-      permission = "FULL_CONTROL"
-    }
-
-    owner {
-      id = data.aws_canonical_user_id.current.id
-    }
-  }
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
